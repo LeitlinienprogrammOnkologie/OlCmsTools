8756d8a3061df2d5e46927bf87476eaca0b02d9d
Gregor Wenzel <81214187+LeitlinienprogrammOnkologie@users.noreply.github.com>
2024-01-19 12:44:12 +0100

    added random recommendation fetcher

---
+++ get_rnd_reco_pg.py
@@ -0,0 +1,58 @@
+import psycopg2
+import random
+from psycopg2 import sql
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("db.ini")
+
+N_RECO = 100
+
+pg_host = config.get("PGSQL", "host")
+pg_db = config.get("PGSQL", "database")
+pg_user = config.get("PGSQL", "user")
+pg_pwd = config.get("PGSQL", "password")
+
+conn = psycopg2.connect(
+    host=pg_host,
+    database=pg_db,
+    user=pg_user,
+    password=pg_pwd)
+
+cur = conn.cursor()
+cur.execute("SELECT uid, short_title FROM ll_data WHERE state = 'published'")
+rows = cur.fetchall()
+cur.close()
+
+recos = {}
+
+result = []
+def get_recos(guideline_data):
+    cur = conn.cursor()
+
+    cur.execute(sql.SQL("SELECT * FROM {table} WHERE {key} = '%s'" % guideline_data[0]).format(
+        table=sql.Identifier("ll_recommendations"),
+        key = sql.Identifier("ll_uid")))
+
+    reco_rows = cur.fetchall()
+    cur.close()
+    return reco_rows
+
+for i in range(N_RECO):
+    guideline_index = i % len(rows)
+    if rows[guideline_index][0] not in recos:
+        recos[rows[guideline_index][0]] = get_recos(rows[guideline_index])
+
+    rnd_reco = recos[rows[guideline_index][0]][random.randint(0, len( recos[rows[guideline_index][0]])) - 1]
+    while rnd_reco in result:
+        rnd_reco = recos[rows[guideline_index][0]][random.randint(0, len(recos[rows[guideline_index][0]])) - 1]
+
+    buffer = list(rnd_reco)
+    buffer.insert(0, rows[guideline_index][1])
+    rnd_reco = tuple(buffer)
+    result.append(rnd_reco)
+
+pass
+
+
+
