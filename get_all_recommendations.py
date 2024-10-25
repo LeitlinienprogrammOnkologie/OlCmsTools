6a9317dd839d62194b8a60f2900346f179ad138d
Gregor Wenzel <81214187+LeitlinienprogrammOnkologie@users.noreply.github.com>
2024-10-25 14:34:13 +0200

    - added a few important columns to extracted recommendations

--- get_all_recommendations.py
+++ get_all_recommendations.py
@@ -1,15 +1,19 @@
+import datetime
+
 from GuidelineService import GuidelineService
 from bs4 import BeautifulSoup
 
 service = GuidelineService()
 
-guideline_list = service.download_guideline_list()
+guideline_list = service.download_guideline_list(state="published")
+guideline_list.extend(service.download_guideline_list(state="private"))
+guideline_list.extend(service.download_guideline_list(state="consulted"))
 
 current_chapter_level = -1
 current_chapter_list = ["", "", ""]
 current_topic = "Anderes"
 
-out_csv = "Leitlinie|Kapitel 1|Kapitel 2|Kapitel 3|Thema|Nummer|Text|Typ|LoE|GoR\n"
+out_csv = "Leitlinie|AWMF-ID|Status|ersion|Jahr|Fullname|Kapitel 1|Kapitel 2|Kapitel 3|Thema|Nummer|Text|Typ|LoE|SIGN|Oxford 2009|Oxford 2011|GRADE|GoR\n"
 
 topic_dict = \
     {
@@ -56,8 +60,24 @@ def get_topic(title):
 
     return "Anderes"
 
-def scan_subsections(subsection_list):
-    global out_csv, current_chapter_list, current_chapter_level, current_topic
+def get_loe_types(loe_list):
+    result = [0, 0, 0, 0]
+    for loe in loe_list:
+        if 'internal_name' in loe['level_of_evidence'] and loe['level_of_evidence']['internal_name'] is not None:
+            name = loe['level_of_evidence']['internal_name']
+            if "SIGN" in name:
+                result[0] += 1
+            elif "Oxford 2006"  in name or "Oxford 2009" in name:
+                result[1] += 1
+            elif "Oxford 2011" in name:
+                result[2] += 1
+            elif "GRADE" in name:
+                result[3] += 1
+
+    return result
+
+def scan_subsections(subsection_list, state):
+    global guideline, out_csv, current_chapter_list, current_chapter_level, current_topic
     current_chapter_level += 1
 
     for subsection in subsection_list:
@@ -78,13 +98,15 @@ def scan_subsections(subsection_list):
             reco_type = subsection['type_of_recommendation']['name'] if ('type_of_recommendation' in subsection and 'name' in subsection['type_of_recommendation']) else ""
             reco_gor = ""
             reco_loe = ""
+            reco_loe_types = [0, 0, 0, 0]
             if "Evidenzbasiert" in reco_type:
                 if "Empfehlung" in reco_type:
                     if 'recommendation_grade' in subsection and subsection['recommendation_grade'] is not None and 'title' in subsection['recommendation_grade'] and subsection['recommendation_grade']['title'] is not None:
                         reco_gor = subsection['recommendation_grade']['title']
                 if 'level_of_evidences' in subsection and subsection['level_of_evidences'] is not None:
                     loe_list = subsection['level_of_evidences']
-                    reco_loe = ",".join([x['level_of_evidence']['title'] for x in loe_list if x['level_of_evidence'] is not None])
+                    reco_loe_types = get_loe_types(loe_list)
+                    #reco_loe_types = ",".join([x['level_of_evidence']['title'] for x in loe_list if x['level_of_evidence'] is not None and 'title' in x['level_of_evidence'] and x['level_of_evidence']['title'] is not None])
             else:
                 if "sollte" in reco_text:
                     reco_gor = "B"
@@ -93,24 +115,33 @@ def scan_subsections(subsection_list):
                 elif "kann" in reco_text or "können" in reco_text:
                     reco_gor = "0"
 
-            reco = [guideline['short_title'], current_chapter_list[0], current_chapter_list[1], current_chapter_list[2], current_topic, subsection['number'].replace(".","_"), reco_text, reco_type, reco_loe, reco_gor]
+            guideline_date = datetime.datetime.fromisoformat(guideline['date'].rstrip('Z'))
+            fullname = "%s V%s (%s)" % (guideline['short_title'], guideline['version'], guideline_date.year)
+            reco = [guideline['short_title'], guideline['awf_registernumber'], state, guideline['version'], str(guideline_date.year), fullname, current_chapter_list[0], current_chapter_list[1], current_chapter_list[2], current_topic, subsection['number'].replace(".","_"), reco_text, reco_type, reco_loe, str(reco_loe_types[0]), str(reco_loe_types[1]), str(reco_loe_types[2]), str(reco_loe_types[3]), reco_gor]
             out_csv += "|".join(reco)+"\n"
         pass
 
         if "subsections" in subsection:
             try:
-                scan_subsections(subsection['subsections'])
+                scan_subsections(subsection['subsections'], state)
             except Exception as err:
                 print(f"Unexpected {err=}, {type(err)=}")
 
     current_chapter_level -= 1
 
+today = datetime.datetime.today()
+
 for guideline in guideline_list:
-    if guideline['state'] == "published" and guideline['guideline_language']['id'] == "de":
-        guideline = service.download_guideline(guideline['id'], load_references=False)
-        #guideline = service.load_guideline(guideline['id'])
-        scan_subsections(guideline['subsections'])
-        f = open("reco_list.csv", "w")
-        f.write(out_csv)
-        f.close()
-        print(guideline['short_title']+" fertig.")
+    #if guideline['state'] == "published" and guideline['guideline_language']['id'] == "de":
+    if guideline['guideline_language']['id'] == "de":
+        state = guideline['state']
+        try:
+            guideline = service.download_guideline(guideline['id'], load_references=False, guideline_state=state)
+            scan_subsections(guideline['subsections'], state)
+            print("%s, state %s fertig." % (guideline['short_title'], state))
+        except:
+            print("%s, state %s nichtr verfügbar." % (guideline['short_title'], state))
+
+f = open("%s-%s-%s_reco_list.csv" % (today.year, today.month, today.day), "w", encoding="utf-8")
+f.write(out_csv)
+f.close()
